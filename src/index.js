9ab99f15df4b49d3e29177f02b8ace06d0ded9e3
SofijaSutton <srlsutton@gmail.com>
2021-01-26 13:02:37 -0500

    adding map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("Hello from JS");
+
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic29maWphc3V0dG9uIiwiYSI6ImNra2VhdmtvNTBiMHgybnF0cXp6MHZxbHUifQ.V1KmmZnX6VUv4UGoO_mMVg';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
